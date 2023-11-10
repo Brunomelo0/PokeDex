42b1303b13509ffa43fc65faf4dea4de23af0552
Brunomelo0 <brunosilvalustosa4@gmail.com>
2023-11-10 14:13:18 -0300

    Adicionando mais gerações

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@ let pokemonSelected = reactive(ref())
 let loading = ref(false)
 
 onMounted(() => {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=302&offset=0")
     .then(res => res.json())
     .then(res => pokemons.value = res.results)
 })
